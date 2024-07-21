f83f9a8b863741d21b2b8bf8f384864fe6787e0c
ChenYv <chenyv19980616@163.com>
2024-07-21 20:33:07 +0800

    更换配置

--- gulimail-coupon/src/main/java/com/atguigu/gulimail/coupon/GulimailCouponApplication.java
+++ gulimail-coupon/src/main/java/com/atguigu/gulimail/coupon/GulimailCouponApplication.java
@@ -1,13 +1,11 @@
 package com.atguigu.gulimail.coupon;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@MapperScan(basePackages = "com.atguigu.gulimail.coupon.dao")
 public class GulimailCouponApplication {
 
     public static void main(String[] args) {
